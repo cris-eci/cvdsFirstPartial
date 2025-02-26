9aad571eba907582bbab36c7e2ad7e4bbdd37ef9
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2025-02-26 07:16:52 -0500

    Add initial project configuration with spring boot

---
+++ README.md
@@ -0,0 +1,5 @@
+# First partial
+
+1. Creamos la estructura del proyecto con maven y Spring boot
+![alt text](image.png)
+
---
+++ src/test/java/edu/eci/cvds/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
